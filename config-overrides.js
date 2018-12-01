c042172b973790723c1ebd6feb3eca2fd0e3c3d4
hanzhecheng <1119216543@qq.com>
2018-12-01 10:26:04 +0800

    update

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,7 @@ function resolve(dir) {
     return path.join(__dirname, '.', dir)
 }
 module.exports = function override(config, env) {
-    //增加别名 方便引用
+    //增加别名 方便文件引用写路径
     config.resolve.alias = {
         '@': resolve('src')
     }
