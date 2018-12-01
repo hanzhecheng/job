8244c423df3d324c7e454dba934b07ea83852b42
hanzhecheng <1119216543@qq.com>
2018-12-01 10:24:52 +0800

    修改webpack配置，增加引用路径别名

--- config-overrides.js
+++ config-overrides.js
@@ -4,6 +4,7 @@ function resolve(dir) {
     return path.join(__dirname, '.', dir)
 }
 module.exports = function override(config, env) {
+    //增加别名 方便引用
     config.resolve.alias = {
         '@': resolve('src')
     }
