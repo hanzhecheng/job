3d8f4ae4f9b545d25322c62d28c7ff8dc67c5233
hanzhecheng <1119216543@qq.com>
2018-12-04 15:41:55 +0800

    优化获取错误信息写法

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -4,7 +4,7 @@ import {
 } from 'antd';
 const methods = ["get", "post"]
 let ajax = axios.create({
-    timeout: 5000
+    timeout: 50000
 })
 const fetch = {}
 ajax.interceptors.request.use(config => {
@@ -13,52 +13,29 @@ ajax.interceptors.request.use(config => {
 }, error => {
     return new Promise.reject(error)
 })
+const errorMsg = {
+    400: "请求错误",
+    401: "未授权，请重新登录",
+    403: "拒绝访问",
+    404: "请求错误,未找到该资源",
+    405: "请求方法未允许",
+    408: "请求超时",
+    500: "服务器端出错",
+    501: "网络未实现",
+    502: "网络错误",
+    503: "服务不可用",
+    504: "网络超时",
+    505: "http版本不支持该请求"
+};
 
 ajax.interceptors.response.use(response => {
     return response
 }, error => {
     // 请求错误时做些事
     if (error && error.response) {
-        switch (error.response.status) {
-            case 400:
-                error.message = '错误请求'
-                break;
-            case 401:
-                error.message = '未授权，请重新登录'
-                break;
-            case 403:
-                error.message = '拒绝访问'
-                break;
-            case 404:
-                error.message = '请求错误,未找到该资源'
-                break;
-            case 405:
-                error.message = '请求方法未允许'
-                break;
-            case 408:
-                error.message = '请求超时'
-                break;
-            case 500:
-                error.message = '服务器端出错'
-                break;
-            case 501:
-                error.message = '网络未实现'
-                break;
-            case 502:
-                error.message = '网络错误'
-                break;
-            case 503:
-                error.message = '服务不可用'
-                break;
-            case 504:
-                error.message = '网络超时'
-                break;
-            case 505:
-                error.message = 'http版本不支持该请求'
-                break;
-            default:
-                error.message = `连接错误${error.response.status}`
-        }
+        let errMsg = errorMsg[error.response.status]
+        error.message = errMsg ? errMsg : `连接错误${error.response.status}`
+
     } else {
         error.message = "连接到服务器失败"
     }
